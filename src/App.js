17016699f01bf9d580f9ac1635fd66fbb74223b2
Felix <barosiofelix@gmail.com>
2022-10-03 16:18:21 +0300

    Completed Assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState("")
+  const [images, setImages] = useState([])
+
   useEffect(() => {
-    console.log("useEffect called");
-  });
+    document.title = text;
+  }, [text])
+
+  useEffect(() => {
+    setTimeout(() => setCount(0), 5000);
+  }, [count])
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random/3")
+      .then((r) => r.json())
+      .then((data) => {
+        setImages(data.message);
+      });
+  }, []);
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <div>
+      <button onClick={() => setCount((count) => count + 1)}>
+        Clicked {count} times
+      </button>
+      <input
+        type="text"
+        placeholder="Type away...."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+
+      <div>
+        {images.map((image) => (
+          <img src={image} alt={image} key={image} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
